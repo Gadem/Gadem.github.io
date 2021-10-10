f239e1f8ad83ffca7be423a9cbbe700edc597d33
Adrian Mora <musica614@gmail.com>
2021-10-09 18:01:42 -0600

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,12 +8,12 @@ window.addEventListener('scroll', function (e) {
 	last_scroll_position = window.scrollY;
 
 	// Scrolling down
-	if (new_scroll_position < last_scroll_position && last_scroll_position > 40) {
+	if (new_scroll_position < last_scroll_position && last_scroll_position > 90) {
 		header.classList.remove("is-visible");
 		header.classList.add("is-hidden");
 
 		// Scrolling up
-	} else if (new_scroll_position > last_scroll_position) {
+	} else if (new_scroll_position > last_scroll_position && last_scroll_position > 89) {
 		header.classList.remove("is-hidden");
 		header.classList.add("is-visible");
 		if (stickyMenu) {
@@ -440,35 +440,6 @@ window.addEventListener('scroll', function (e) {
     init();
 })(window.publiiThemeMenuConfig);
 
-// Load comments
-var comments = document.getElementById("js-comments");  
-   if (comments) {
-      comments.addEventListener("click", function() {   
-          comments.classList.toggle("is-hidden");      
-             var container = document.getElementById("js-comments__inner");   
-             container.classList.toggle("is-visible");  
-      });
- }
-
-// Load search input area
-var searchButton = document.querySelector(".js-search-btn");
-    searchOverlay = document.querySelector(".js-search-overlay");
-    searchClose = document.querySelector(".js-search-close");
-    searchInput = document.querySelector(".js-search-input");
-
-if (searchButton) {
-    searchButton.addEventListener("click", function () {        
-        searchOverlay.classList.add("expanded");
-        setTimeout(function() { 
-            searchInput.focus(); 
-        }, 60);        
-    });
-    
-    searchClose.addEventListener("click", function () {
-        searchOverlay.classList.remove('expanded');
-    });
-}
-
 // Share buttons pop-up
 (function () {
     // share popup
@@ -529,74 +500,31 @@ if (searchButton) {
     }
 })();
 
-// Back to Top - by CodyHouse.co on MIT license
-(function(){    
-	var backTop = document.getElementsByClassName('js-footer__bttop')[0],		
-		offset = 600,		
-		offsetOpacity = 1200,
-		scrollDuration = 50,
-		scrolling = false;
-	if( backTop ) {		
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
-			}
-		});
-		backTop.addEventListener('click', function(event) {
-			event.preventDefault();
-			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : scrollTop(scrollDuration);
-		});
-	}
+// Load search input area
+var searchButton = document.querySelector('.js-search-btn');
+    searchOverlay = document.querySelector('.js-search-overlay');
+    searchInput = document.querySelector('.js-search-input');
 
-	function checkBackToTop() {
-		var windowTop = window.scrollY || document.documentElement.scrollTop;
-		( windowTop > offset ) ? addClass(backTop, 'footer__bttop--show') : removeClass(backTop, 'footer__bttop--show', 'footer__bttop--fade-out');
-		( windowTop > offsetOpacity ) && addClass(backTop, 'footer__bttop--fade-out');
-		scrolling = false;
-	}
-	
-	function scrollTop(duration) {
-	    var start = window.scrollY || document.documentElement.scrollTop,
-	        currentTime = null;
-	        
-	    var animateScroll = function(timestamp){
-	    	if (!currentTime) currentTime = timestamp;        
-	        var progress = timestamp - currentTime;
-	        var val = Math.max(Math.easeInOutQuad(progress, start, -start, duration), 0);
-	        window.scrollTo(0, val);
-	        if(progress < duration) {
-	            window.requestAnimationFrame(animateScroll);
-	        }
-	    };
+if (searchButton) {
+    searchButton.addEventListener('click', function () {        
+        searchOverlay.classList.toggle('expanded');
+       
+        setTimeout(function() {
+            if (searchOverlay.classList.contains('expanded')) {
+                searchInput.focus();
+            }
+        }, 60);        
+    });
 
-	    window.requestAnimationFrame(animateScroll);
-	}
+    searchOverlay.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
 
-	Math.easeInOutQuad = function (t, b, c, d) {
- 		t /= d/2;
-		if (t < 1) return c/2*t*t + b;
-		t--;
-		return -c/2 * (t*(t-2) - 1) + b;
-	};
-    
-	function hasClass(el, className) {
-	  	if (el.classList) return el.classList.contains(className);
-	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-	}
-	function addClass(el, className) {
-		var classList = className.split(' ');
-	 	if (el.classList) el.classList.add(classList[0]);
-	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
-	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
-	}
-	function removeClass(el, className) {
-		var classList = className.split(' ');
-	  	if (el.classList) el.classList.remove(classList[0]);	
-	  	else if(hasClass(el, classList[0])) {
-	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
-	  		el.className=el.className.replace(reg, ' ');
-	  	}
-	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
-	}
-})();
+    searchButton.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+
+    document.body.addEventListener('click', function () {
+        searchOverlay.classList.remove('expanded');
+    });
+}
